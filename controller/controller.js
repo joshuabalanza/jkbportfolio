c1061c2ca3d9ba983b9c97313184d9b3f899859b
Joshua Kim Balanza <joshuabalanza@gmail.com>
2023-09-30 00:00:38 +0800

    Add Controller

---
+++ controller/controller.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    document.getElementById('UserAge').innerText = MyAge();
+});
+
+
+function MyAge(){
+    var today = new Date();
+    var birthDate = new Date('1997/11/08');
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
