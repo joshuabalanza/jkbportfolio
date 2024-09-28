d330f85bf5302fa2256ef8668a1e7a6de3ebdb1a
Joshua Kim Balanza <joshuabalanza@gmail.com>
2024-09-28 20:13:01 +0800

    test

--- controller/controller.js
+++ controller/controller.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 
 function MyAge(){
     var today = new Date();
-    var birthDate = new Date('1997/11/08');
+    var birthDate = new Date('1999/11/08');
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
